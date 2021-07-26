f38f91136bc7644d952880a5874137a008061efb
Joshi-Hiroshi <68434268+Joshi-Hiroshi@users.noreply.github.com>
2021-07-26 16:19:37 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect , flash , url_for
+
+
+app=Flask(__name__)
+app.config['SECRET_KEY'] = 'babaBlackSheep$123'
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+app.run(debug=True)
