63a4615ca727ed02b4318ed04ee54f44cf6ba1c1
Joshi-Hiroshi <68434268+Joshi-Hiroshi@users.noreply.github.com>
2021-07-26 16:24:25 +0530

    added comments to the code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect , flash , url_for
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'babaBlackSheep$123'
 
-
+#Main Home Route
 @app.route("/")
 def home():
     return render_template("index.html")
