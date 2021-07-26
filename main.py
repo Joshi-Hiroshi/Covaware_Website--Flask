d4613360e3c45fd10958ea749ac73e07390902c1
Joshi-Hiroshi <68434268+Joshi-Hiroshi@users.noreply.github.com>
2021-07-26 20:45:29 +0530

    Added the world map , which gets the data and stuff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect , flash , url_for
-
+from flask import Flask, render_template, request, redirect , flash , url_for,json,jsonify
+import data_file
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'babaBlackSheep$123'
@@ -8,5 +8,24 @@ app.config['SECRET_KEY'] = 'babaBlackSheep$123'
 @app.route("/")
 def home():
     return render_template("index.html")
+
+def data_in(Country, State=None):
+    index_in, data_in_c = data_file.data_Confirmed(Country, State)
+    data_in_r = data_file.data_Recovered(Country, State)
+    data_in_d = data_file.data_Deaths(Country, State)
+
+    return [index_in, data_in_c, data_in_r, data_in_d]
+
+
+@app.route('/world_map', methods=['GET'])
+def world_wide():
+    list_of_data = data_file.data_of_world_wide()
+    index = [['Country', 'Confirmed', 'Recovered']]
+    for i in list_of_data:
+        index.append(i)
+    data_json = json.dumps(index)
+    # print(index[0:2], type(index))
+
+    return render_template('worldwide.html', list_data=data_json)
 
 app.run(debug=True)
